e1af5ac5061874b45321048704a3a0d636a82d32
zoryana668 <zoryana668@gmail.com>
2026-01-08 17:40:19 +0200

    commit

---
+++ src/data/course.ts
@@ -0,0 +1,11 @@
+import type {CourseTitleType} from "../models/coursesTitleType.ts";
+
+
+export const coursesTitleArray:CourseTitleType[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
