5cf4aa1fcf1a6a2e6a0eae50e0449bb8360b9ad1
zoryana668 <zoryana668@gmail.com>
2026-01-08 17:34:07 +0200

    commit

---
+++ src/components/course/course.tsx
@@ -0,0 +1,13 @@
+import type {CourseTitleType} from "../../models/coursesTitleType.ts";
+
+
+const courseTitleArray:CourseTitleType[] = [
+    'JavaScript Complex',
+    'Java Complex',
+    'Python Complex',
+    'QA Complex',
+    'Fullstack',
+    'Frontend'
+];
+
+export default Course;
---
+++ src/components/courses/courses.tsx
@@ -0,0 +1,11 @@
+import type {FC} from "react";
+
+const Courses:FC = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Courses;
