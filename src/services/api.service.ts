ef23844906f9fd205d2ac06a00dedd0f416140ee
zoryana668 <zoryana668@gmail.com>
2026-01-19 15:09:49 +0200

    adding .env file to learning files

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,13 +1,14 @@
 import type {IUser} from "../models/IUser.ts";
 
+
 const getUsers = async ():Promise<IUser[]> => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch(import.meta.env.VITE_API_URL)
         .then(value => value.json())
     return users;
 }
 
 const getUser = async(id: string):Promise<IUser> => {
-    const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const user = await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then(value => value.json())
     return user;
 }
