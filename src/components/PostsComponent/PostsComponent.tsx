6434873553b7b2086e3eb76441ec037cf6d7b96a
zoryana668 <zoryana668@gmail.com>
2026-01-22 15:37:01 +0200

    updates

--- src/components/PostsComponent/PostsComponent.tsx
+++ src/components/PostsComponent/PostsComponent.tsx
@@ -18,7 +18,7 @@ const PostsComponent = () => {
 
     return (
         <div>
-            postsArray.map((post) => <PostComponent key={post.id} item={post}/>)
+            postsArray.map(post => (<PostComponent key={post.id} item={post}/>))
         </div>
     );
 };
