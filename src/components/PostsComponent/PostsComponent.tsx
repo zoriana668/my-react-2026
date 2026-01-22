2820326906eaf8d555bc2c7defd1d37aed31ba84
zoryana668 <zoryana668@gmail.com>
2026-01-22 15:25:31 +0200

    updates

--- src/components/PostsComponent/PostsComponent.tsx
+++ src/components/PostsComponent/PostsComponent.tsx
@@ -1,4 +1,17 @@
+import {useEffect} from "react";
+import {getPosts} from "../../service/api.service.ts";
+
 const PostsComponent = () => {
+
+    const [postsArray, setPost] = useEffect([]);
+
+    useEffect(() => {
+        getPosts()
+            .then(response)
+            setPost(response);
+    }, []);
+
+
     return (
         <div>
 
