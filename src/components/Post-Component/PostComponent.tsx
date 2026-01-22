70815538b6218a424cf853cd453fcbb414d953e7
zoryana668 <zoryana668@gmail.com>
2026-01-22 16:22:13 +0200

    adding some additional styles to Post Component

--- src/components/Post-Component/PostComponent.tsx
+++ src/components/Post-Component/PostComponent.tsx
@@ -1,17 +1,25 @@
 import type {IPost} from "../../models/IPost.ts";
 import type {FC} from "react";
 
+import './PostComponent.css'
+
 type PostPropType = {
     item: IPost
 }
 
-const PostComponent:FC<PostPropType> = ({item}) => {
+const PostComponent:FC<PostPropType> = ({item: {id, title, body}}) => {
 
     return (
         <div>
             {
-                <div className='mb-10 p-7 bg-green-800' key={item.id}>
-                    {`Post ID - ${item.id} Post title - ${item.title}`}</div>
+                <div className='mb-10 p-7 bg-green-800' key={id}>
+                    <div><span>Post ID</span> - {id}</div>
+
+                    <div><span>Post title</span> - {title}</div>
+
+                    <div><span>Content</span> - {body}</div>
+
+                </div>
 
             }
         </div>
