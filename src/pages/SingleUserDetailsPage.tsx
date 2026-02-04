162c5af6d37992cce6e4ce4ea8a3365e0fadcc44
zoryana668 <zoryana668@gmail.com>
2026-02-04 22:06:49 +0200

    creatingSingleUserDetailsComponent

--- src/pages/SingleUserDetailsPage.tsx
+++ src/pages/SingleUserDetailsPage.tsx
@@ -1,14 +1,11 @@
-import {useLocation} from "react-router-dom";
-import type {IUser} from "../models/IUser.ts";
+import SingleUserDetailsComponent from "../components/singleUserDetails/SingleUserDetailsComponent.tsx";
 
 const SingleUserDetailsPage = () => {
 
-    const {state} = useLocation();
-    const user = state as IUser;
 
     return (
         <div>
-            {user.email}
+            <SingleUserDetailsComponent/>
         </div>
     );
 };
