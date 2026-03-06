1d4a7d0af18320da654312a3d607f9165821667a
zoryana668 <zoryana668@gmail.com>
2026-03-06 16:28:30 +0200

    updates (adding UsersPage)

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,5 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout.tsx";
 
 export const routes = createBrowserRouter([
-
-])
+    {path: '/', element: <MainLayout/>}
+]);
