8e048f1986de2169647d2aed401ad7218bdf6eec
zoryana668 <zoryana668@gmail.com>
2026-03-06 16:24:31 +0200

    updates (adding router)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {RouterProvider} from "react-router-dom";
+import {routes} from "./router/routes.tsx";
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={routes}/>)
+
