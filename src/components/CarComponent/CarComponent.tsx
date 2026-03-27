d43976ebb9164af292cefb18c29ff8180ee8a77a
zoryana668 <zoryana668@gmail.com>
2026-03-27 18:36:48 +0200

    updates

--- src/components/CarComponent/CarComponent.tsx
+++ src/components/CarComponent/CarComponent.tsx
@@ -1,15 +1,16 @@
 import type {ICar} from "../../models/ICar.ts";
+import type {FC} from "react";
 
 type CarPropType = {
     item: ICar
 }
 
-const CarComponent = () => {
+const CarComponent:FC<CarPropType> = ({item}) => {
 
 
     return (
         <div>
-
+            {item.id} {item.brand} - {item.price}
         </div>
     );
 };
