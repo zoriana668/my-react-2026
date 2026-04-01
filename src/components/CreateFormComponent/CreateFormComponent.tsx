538837889c5af596450d1f786cfbcdbd96510ef4
zoryana668 <zoryana668@gmail.com>
2026-04-01 16:26:10 +0300

    updates - adding errors block to input fields and adding some stylization to them

--- src/components/CreateFormComponent/CreateFormComponent.tsx
+++ src/components/CreateFormComponent/CreateFormComponent.tsx
@@ -3,6 +3,7 @@ import {carValidator} from "../../validators/car.validator.ts";
 import {joiResolver} from "@hookform/resolvers/joi";
 
 import './CreateFormComponent.css';
+import '../Menu/General.css';
 
 interface IFormProps {
     brand: string,
@@ -12,7 +13,7 @@ interface IFormProps {
 
 const CreateFormComponent = () => {
 
-    const {handleSubmit, register, formState: {isValid}} = useForm<IFormProps>({mode: 'all', resolver: joiResolver(carValidator)});
+    const {handleSubmit, register, formState: {errors, isValid}} = useForm<IFormProps>({mode: 'all', resolver: joiResolver(carValidator)});
 
     const customHandler = (formDataProps:IFormProps) => {
         console.log(formDataProps)
@@ -26,9 +27,31 @@ const CreateFormComponent = () => {
                 <form onSubmit={handleSubmit(customHandler)} className={'create-form'}>
                     <h3 className={'form-heading'}>Create a car</h3>
 
-                    <input type='text' className={'input-field input-brand'} {...register('brand')}/>
-                    <input type='number' className={'input-field input-price'} {...register('price')}/>
-                    <input type='number' className={'input-field input-year'} {...register('year')}/>
+                    <div className={'input-wrapper'}>
+                        <label htmlFor={'brand'}>
+                            Type a brand
+                        </label>
+                        <input type='text' id='brand' className={'input-field input-brand'} {...register('brand')}/>
+                        {errors.brand && <div className={'error-message'}>{errors.brand.message}</div>}
+                    </div>
+
+
+                    <div className={'input-wrapper'}>
+                        <label htmlFor={'price'}>
+                            Type a price
+                        </label>
+                        <input type='number' id='price' className={'input-field input-price'} {...register('price')}/>
+                        {errors.price && <div className={'error-message'}>{errors.price.message}</div>}
+                    </div>
+
+                    <div className={'input-wrapper'}>
+                        <label htmlFor={'year'}>
+                            Type a year
+                        </label>
+                        <input type='number' id='year' className={'input-field input-year'} {...register('year')}/>
+                        {errors.year && <div className={'error-message'}>{errors.year.message}</div>}
+                    </div>
+
                     <button className={'btn-create'} disabled={!isValid}>Create</button>
                 </form>
             </div>
